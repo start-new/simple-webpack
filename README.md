3181cdd476d154cd86d5530fa458894f04b0380d
Thierry Chappuis <tchappui@gmail.com>
2021-10-15 16:23:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Webpack Project Template
+
+This is a simple webpack project template aimed at being use by the cookiecutter
+tool.
---
+++ {{ cookiecutter.project_slug }}/webpack/webpack.common.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, '../src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'js/[name].[chunkhash].bundle.js',
+        clean: true
+    },
+    module: {
+        rules: [ 
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.(scss|css)$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: false,
+            title: 'zgzig'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "./css/[name].[contenthash].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+};
